f52d5f8358d622adecabbb366e4ffd7a87cc602c
MohamedAENasser <mohammed_abdelnasser@yahoo.com>
2023-06-04 18:23:43 +0300

    Initial commit

---
+++ MoviesApp/MoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Mohamed Abd ElNasser on 04/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
